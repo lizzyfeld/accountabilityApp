c72c48d0676806d2de5e618fb455936d188f551b
lizzyfeld <lizfeld@stanford.edu>
2022-05-08 21:11:52 -0700

    first commit

---
+++ Views/MainPage.js
@@ -0,0 +1,41 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+// import style from './mainPage.css'
+
+{/* <link rel="stylesheet" href="mainPage.css"></link> */}
+
+
+export default function MainPage() {
+  return (
+    <View style={style.container}>
+      <Text style={style.header}>
+        Events
+      </Text>
+      <Pressable style={style.button}>
+        <Text>Create Event</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'pink',
+  },
+});
